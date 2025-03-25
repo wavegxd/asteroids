9e338409ae1c5c9ecc7d640163f9599ef22888d6
wavegxd <roa.michael5@gmail.com>
2025-03-25 12:15:59 -0700

    updating main funtion

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import pygame
 from constants import *
 
-pygame.init()
-
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
 
 def main():
-    while pygame.get_init:
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
         pygame.display.flip()
     print("Starting Asteroids!")
